9ba0aeee1f3759069951051f309bfa5da4ac8657
Vijayenthiran Subramaniam <vijayenthiran.s@gmail.com>
2015-02-02 20:52:31 +0530

    Update L5P3_Finding power using recursion.py

--- L5P3_Finding power using recursion.py
+++ L5P3_Finding power using recursion.py
@@ -18,3 +18,21 @@ def recurPowerNew(base, exp):
         return recurPowerNew(base*base,exp/2)
     else:
         return base*recurPowerNew(base,exp-1)
+
+
+#Write a function recurPower(base, exp) which computes baseexp by recursively calling itself to solve a smaller version of the same problem, and then multiplying the result by base to solve the initial problem.
+
+#This function should take in two values - base can be a float or an integer; exp will be an integer ≥0. It should return one numerical value. Your code must be recursive - use of the ** operator or looping constructs is not allowed.
+
+def recurPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+ 
+    returns: int or float, base^exp
+    '''
+    # Your code here
+    if exp == 0:
+        return 1
+    else:
+        return base * recurPower(base,exp-1)
